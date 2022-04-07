c29cf7ee38e96059702c1a5efd1c97f771930fc2
benchy-data <cs-21-pt-9-01@hotmail.com>
2022-04-07 11:36:58 +0000

    Test setup

--- DockerRunMonolithService.sh
+++ DockerRunMonolithService.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t petstore-monolith .
 
-docker run --rm --add-host=host.docker.internal:host-gateway -p 8000:80 petstore-monolith
+docker run --rm --add-host=host.docker.internal:host-gateway -p 8080:8080 petstore-monolith
 wait
