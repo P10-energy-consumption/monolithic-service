e9144936011192249f37faa051bba6f09a1dd032
GarridoY <dgmn17@student.aau.dk>
2022-05-11 11:49:58 +0200

    Added name to docker container + fixed single benchmark

--- DockerBuildAndRun.sh
+++ DockerBuildAndRun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t petstore-monolith .
 
-docker run --rm --add-host=host.docker.internal:host-gateway -p 8080:8080 petstore-monolith
+docker run --rm --add-host=host.docker.internal:host-gateway --name petstore-monolith -p 8080:8080 petstore-monolith &
